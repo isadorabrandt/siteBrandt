ec508aa2e4119320e8757c22458284328c3e07f7
isadorabrandt <luzcollabb@gmail.com>
2026-05-21 15:39:00 -0300

    site Brandt

---
+++ script.js
@@ -0,0 +1,37 @@
+document.querySelectorAll(".card-img").forEach(carrossel => {
+    let imgs = carrossel.querySelectorAll("img");
+    let index = 0;
+
+    carrossel.querySelector(".next").onclick = () => {
+        imgs[index].classList.remove("active");
+        index = (index + 1) % imgs.length;
+        imgs[index].classList.add("active");
+    };
+
+    carrossel.querySelector(".prev").onclick = () => {
+        imgs[index].classList.remove("active");
+        index = (index - 1 + imgs.length) % imgs.length;
+        imgs[index].classList.add("active");
+    };
+});
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-menu a");
+
+window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop - 100;
+        if (scrollY >= sectionTop) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove("active");
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+    });
+});
